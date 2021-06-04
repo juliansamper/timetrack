0fd0bc5c2db43ac81ea956d7a6c4c6a28d7acc02
unknown <juliansamper@gmail.com>
2021-06-04 09:31:38 -0500

    nodemon config

--- src/connectors/MongoDBConnector.ts
+++ src/connectors/MongoDBConnector.ts
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const { LudyConsole } = require("../utils/LudyConsole");
 
-const uri: string = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@timetrackcluster0.ktn9n.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+// const uri: string = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@timetrackcluster0.ktn9n.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+const uri: string = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@timetrackcluster1.ktn9n.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
 const db = mongoose.connect(uri,
     { useNewUrlParser: true, useUnifiedTopology: true }
